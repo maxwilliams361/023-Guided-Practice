6c20d3efb26e7ead0b02178bae1faf4c37db9916
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2021-01-12 09:14:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+let correct_username = 'codeNation';
+let correct_email = 'codenation@gmail.com';
+let correct_password = '1234';
+
+$(".login").click(function(){
+let usernameInput=$(".username").val();
+let passwordInput= $(".password").val();
+
+
+});
+
+//Independent Practice
